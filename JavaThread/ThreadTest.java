7d7f080674c63fa1df9d1ad4721325005e21213a
chunyangjia <brianjia1123@gmail.com>
2020-10-04 19:40:08 -0400

    1 commit

---
+++ JavaThread/ThreadTest.java
@@ -0,0 +1,26 @@
+public class ThreadTest {
+    public static void main(String[] args) {
+        //3. create a  MyThread class instance
+        MyThread t1 = new MyThread();
+        //4. start
+        t1.start();
+        for (int i = 0; i < 100; i++) {
+            if(i%2 != 0){
+                System.out.println(i+"*****");
+            }
+        }
+    }
+}
+
+//1. a class extending the Thread class
+class MyThread extends Thread {
+    //2. re-write the run method
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            if(i%2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
