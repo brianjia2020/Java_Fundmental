b1398a6dcedd496ae983c57bed9c76111a51d110
chunyangjia <brianjia1123@gmail.com>
2020-10-04 19:46:11 -0400

    2 commit

--- JavaThread/ThreadTest.java
+++ JavaThread/ThreadTest.java
@@ -6,7 +6,7 @@ public class ThreadTest {
         t1.start();
         for (int i = 0; i < 100; i++) {
             if(i%2 != 0){
-                System.out.println(i+"*****");
+                System.out.println(Thread.currentThread().getName()+":"+i+"*****");
             }
         }
     }
@@ -19,7 +19,7 @@ class MyThread extends Thread {
     public void run() {
         for (int i = 0; i < 100; i++) {
             if(i%2 == 0){
-                System.out.println(i);
+                System.out.println(Thread.currentThread().getName()+":"+i);
             }
         }
     }
