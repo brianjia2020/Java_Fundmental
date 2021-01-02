d15b184e29db96e85f6f62956625557b5337df75
chunyangjia <brianjia1123@gmail.com>
2021-01-02 13:02:38 -0500

    new stuff

---
+++ src/JavaOOP/OOPTest.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * 1. java class and class elements: attribute, method, constructor, inner class, code block
+ * 2. OOP inheritance, encapsulation, polymorphism
+ * 3. other key words: this,super,static,final,abstract,interface,package,import
+ *
+ * 4. class: is a general abstraction of one kind of things
+ *    instance: a realized existing object(a real implementation of class)
+ */
+public class OOPTest {
+    
+}
