f2eb54f250bcb871ba0ddb5ca8b56c674140c723
chunyangjia <brianjia1123@gmail.com>
2021-01-02 10:14:06 -0500

    new stuff

--- src/JavaAlgorithm/ArrayExceptionTest.java
+++ src/JavaAlgorithm/ArrayExceptionTest.java
@@ -4,5 +4,8 @@
  * 2. nullPointerException
  */
 public class ArrayExceptionTest {
-
+    public static void main(String[] args) {
+        int[] arr1 = new int[4];
+        System.out.println(arr1[5]);
+    }
 }
