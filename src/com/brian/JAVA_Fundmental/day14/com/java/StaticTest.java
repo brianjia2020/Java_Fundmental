8a4abaa699bf538f867509f30c07f63e424131eb
chunyangjia <brianjia1123@gmail.com>
2021-03-01 20:22:51 -0500

    new stuff

---
+++ src/com/brian/JAVA_Fundmental/day14/com/java/StaticTest.java
@@ -0,0 +1,36 @@
+package com.brian.JAVA_Fundmental.day14.com.java;
+
+
+/**
+ * static:
+ *
+ * 1. can be used to describe mostly the structures of class structure
+ *    :attribute
+ *    :code blocks
+ *    :method
+ *    :inner class
+ * 2. using static to describe attributes
+ *    : static attribute is shared by all the instances of the object
+ *    : non-static attribute belongs to specific instance of object
+ *
+ * static attributes are loaded when class are loaded
+ *
+ * 3. using static to describe code blocks
+ */
+public class StaticTest {
+    public static void main(String[] args) {
+        Chinese c1 = new Chinese();
+        c1.age = 10;
+        c1.name = "Brian";
+        Chinese.nation = "China";
+        System.out.println(c1);
+
+
+    }
+}
+
+class Chinese {
+    String name;
+    int age;
+    static String nation;
+}
